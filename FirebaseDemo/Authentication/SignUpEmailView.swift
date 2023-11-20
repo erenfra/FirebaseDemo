4afbf480bcc8d681f5dcd74b3bf08189459ea261
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-11-20 13:36:45 -0600

    More features added

---
+++ FirebaseDemo/Authentication/SignUpEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpEmailView.swift
+//  FirebaseDemo
+//
+//  Created by Renato Oliveira Fraga on 11/2/23.
+//
+
+import SwiftUI
+
+struct SignUpEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpEmailView()
+}
